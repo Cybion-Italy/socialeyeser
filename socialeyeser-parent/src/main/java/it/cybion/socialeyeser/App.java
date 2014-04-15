bee6ede88de64eeb1e598e3fc16bb1763ab6bb91
Matteo Moci <mox601@libero.it>
2014-04-15 11:09:50 +0200

    initial import

---
+++ socialeyeser-parent/src/main/java/it/cybion/socialeyeser/App.java
@@ -0,0 +1,13 @@
+package it.cybion.socialeyeser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ socialeyeser-parent/src/test/java/it/cybion/socialeyeser/AppTest.java
@@ -0,0 +1,4 @@
+package it.cybion.socialeyeser;
+
+public class AppTest {
+}

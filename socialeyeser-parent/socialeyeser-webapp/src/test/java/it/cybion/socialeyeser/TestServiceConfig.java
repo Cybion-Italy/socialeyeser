d05cdfb5ee83f0cfe0a3a7a91ad5299304946f89
serxhio <serxhiodaja@gmail.com>
2014-05-09 18:40:30 +0200

    updated deps. added first test with jetty

---
+++ socialeyeser-parent/socialeyeser-webapp/src/test/java/it/cybion/socialeyeser/TestServiceConfig.java
@@ -0,0 +1,26 @@
+package it.cybion.socialeyeser;
+
+import javax.servlet.ServletContextEvent;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+
+
+/**
+ * @author Matteo Moci ( matteo (dot) moci (at) gmail (dot) com )
+ */
+public class TestServiceConfig extends GuiceServletContextListener {
+    
+    @Override
+    protected Injector getInjector() {
+        return Guice.createInjector(
+                new TestJerseyServletModule()
+                );
+    }
+    
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        
+    }
+}

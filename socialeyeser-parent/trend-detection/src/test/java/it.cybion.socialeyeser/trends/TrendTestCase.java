c7d808b04eac1d75dc8d3b2a2574ff605b90d8c5
Matteo Moci <mox601@libero.it>
2014-05-13 17:24:47 +0200

    first minimal trend detector test. related to #3

---
+++ socialeyeser-parent/trend-detection/src/test/java/it.cybion.socialeyeser/trends/TrendTestCase.java
@@ -0,0 +1,159 @@
+package it.cybion.socialeyeser;
+
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author Matteo Moci ( matteo (dot) moci (at) gmail (dot) com )
+ */
+public class TrendTestCase {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TrendTestCase.class);
+
+    @Test
+    public void shouldTestSingletonEventBus() throws Exception {
+
+        final EventBus eventBus = new EventBus();
+        final TweetArrivedListener tweetArrivedListener = new TweetArrivedListener();
+
+        eventBus.register(tweetArrivedListener);
+
+        final long tenMsecs = 10L;
+        final TweetStream tweetStream = new TweetStream(tenMsecs, eventBus);
+
+        final Thread producerThread = new Thread(tweetStream);
+        producerThread.start();
+
+        Thread.sleep(1000L);
+
+        tweetStream.stop();
+        producerThread.join();
+
+    }
+
+    private class TweetArrivedListener {
+
+        private final Queue<DateTime> lastRequests;
+
+        private int maxTweetsPerWindow;
+
+        private int windowSizeMsecs;
+
+        private TweetArrivedListener() {
+
+            this.lastRequests = new LinkedList<DateTime>();
+            this.maxTweetsPerWindow = 10;
+            this.windowSizeMsecs = 100;
+
+        }
+
+        @Subscribe
+        public void recordTweet(final TweetArrived event) {
+
+            final DateTime timeStamp = event.getTimeStamp();
+            //            LOGGER.info("tweet arrived: '" + event.getTweetId() + "' at '" + timeStamp.getMillis() +
+            //                        "'");
+
+            boolean trendDetected = areLastEventsEnoughForTrend(timeStamp);
+
+            if (trendDetected) {
+                LOGGER.info("it's trending!");
+            } else {
+                LOGGER.info("nothing to say");
+            }
+
+        }
+
+        private boolean areLastEventsEnoughForTrend(final DateTime timestamp) {
+
+            //update
+            this.lastRequests.add(timestamp);
+
+            //check
+            final int size = this.lastRequests.size();
+
+            if (size >= maxTweetsPerWindow) {
+                final DateTime removed = this.lastRequests.poll();
+            }
+
+            final DateTime oldest = this.lastRequests.peek();
+
+            if (oldest != null) {
+
+                Interval interval = new Interval(oldest, timestamp);
+                if (interval.toDurationMillis() >= windowSizeMsecs) {
+                    LOGGER.info("interval " + interval.toDurationMillis() + " msec");
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+
+    private class TweetArrived {
+
+        private String tweetId;
+
+        private DateTime timeStamp;
+
+        public TweetArrived(final String tweetId, final DateTime timeStamp) {
+
+            this.tweetId = tweetId;
+            this.timeStamp = timeStamp;
+        }
+
+        public String getTweetId() {
+
+            return tweetId;
+        }
+
+        public DateTime getTimeStamp() {
+
+            return timeStamp;
+        }
+    }
+
+    private class TweetStream implements Runnable {
+
+        private final EventBus eventBus;
+
+        private Long pauseBetweenTweets;
+
+        private boolean running = true;
+
+        public TweetStream(Long pauseBetweenTweets, EventBus eventBus) {
+
+            this.pauseBetweenTweets = pauseBetweenTweets;
+            this.eventBus = eventBus;
+        }
+
+        @Override
+        public void run() {
+
+            while (running) {
+                final DateTime now = new DateTime();
+                this.eventBus.post(new TweetArrived("" + now.getMillis(), now));
+                try {
+                    Thread.sleep(pauseBetweenTweets);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void stop() {
+
+            this.running = false;
+        }
+    }
+}

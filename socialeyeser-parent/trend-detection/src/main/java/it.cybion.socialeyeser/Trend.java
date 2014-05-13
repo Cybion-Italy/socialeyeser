64a0fc1619cf60ac9670d78b83cebbe19dd6e29a
Matteo Moci <mox601@libero.it>
2014-05-13 15:18:38 +0200

    added packages

--- socialeyeser-parent/trend-detection/src/main/java/it.cybion.socialeyeser/Trend.java
+++ socialeyeser-parent/trend-detection/src/main/java/it.cybion.socialeyeser/Trend.java
@@ -1,3 +1,5 @@
+package it.cybion.socialeyeser;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- socialeyeser-parent/trend-detection/src/test/java/it.cybion.socialeyeser/TrendTestCase.java
+++ socialeyeser-parent/trend-detection/src/test/java/it.cybion.socialeyeser/TrendTestCase.java
@@ -1,3 +1,5 @@
+package it.cybion.socialeyeser;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;

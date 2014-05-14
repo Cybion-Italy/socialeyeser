698f6802044de71bfb1ea7d80ef96cdb8efd02dd
Matteo Moci <mox601@libero.it>
2014-05-14 10:12:52 +0200

    minor

--- socialeyeser-parent/influence/influence-endpoint/src/test/java/it/cybion/socialeyeser/influence/resources/InfluencersResourceTestCase.java
+++ socialeyeser-parent/influence/influence-endpoint/src/test/java/it/cybion/socialeyeser/influence/resources/InfluencersResourceTestCase.java
@@ -6,17 +6,18 @@ import it.cybion.commons.web.http.CybionHttpClient;
 import it.cybion.commons.web.http.exceptions.CybionHttpException;
 import it.cybion.commons.web.responses.ExternalStringResponse;
 import it.cybion.commons.web.responses.ResponseStatus;
-import it.cybion.socialeyeser.influence.utils.JettyServer;
 import it.cybion.socialeyeser.influence.guice.TestServiceConfig;
+import it.cybion.socialeyeser.influence.utils.JettyServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import javax.ws.rs.core.MediaType;
 import java.util.Map;
+
+import static org.testng.Assert.assertTrue;
 
 public class InfluencersResourceTestCase extends JettyServer {
     
@@ -52,28 +53,28 @@ public class InfluencersResourceTestCase extends JettyServer {
         stringResponse = CybionHttpClient.performGet(url, requestHeaderMap);
         
         LOGGER.info("response body: " + stringResponse.getObject());
-        Assert.assertTrue(ResponseStatus.OK == stringResponse.getStatus(), "Unexpected result: "
-                + stringResponse.getMessage());
+        assertTrue(ResponseStatus.OK == stringResponse.getStatus(),
+                "Unexpected result: " + stringResponse.getMessage());
         
         url = super.baseUri + "twitter?userId=1234&followers=-100";
         stringResponse = CybionHttpClient.performGet(url, requestHeaderMap);
         
         LOGGER.info("response body: " + stringResponse.getObject());
-        Assert.assertTrue(ResponseStatus.NOK == stringResponse.getStatus(), "Unexpected result: "
-                + stringResponse.getMessage());
+        assertTrue(ResponseStatus.NOK == stringResponse.getStatus(),
+                "Unexpected result: " + stringResponse.getMessage());
         
         url = super.baseUri + "twitter?userId=1234";
         stringResponse = CybionHttpClient.performGet(url, requestHeaderMap);
         
         LOGGER.info("response body: " + stringResponse.getObject());
-        Assert.assertTrue(ResponseStatus.NOK == stringResponse.getStatus(), "Unexpected result: "
-                + stringResponse.getMessage());
+        assertTrue(ResponseStatus.NOK == stringResponse.getStatus(),
+                "Unexpected result: " + stringResponse.getMessage());
         
         url = super.baseUri + "twitter/";
         stringResponse = CybionHttpClient.performGet(url, requestHeaderMap);
         
         LOGGER.info("response body: " + stringResponse.getObject());
-        Assert.assertTrue(ResponseStatus.NOK == stringResponse.getStatus(), "Unexpected result: "
-                + stringResponse.getMessage());
+        assertTrue(ResponseStatus.NOK == stringResponse.getStatus(),
+                "Unexpected result: " + stringResponse.getMessage());
     }
 }

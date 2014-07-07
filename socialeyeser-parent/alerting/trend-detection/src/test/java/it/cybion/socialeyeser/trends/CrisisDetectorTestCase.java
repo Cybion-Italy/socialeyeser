6178cfacd438f0f58225bfe5d8e8989cd416df78
Matteo Moci <mox601@libero.it>
2014-07-07 11:26:58 +0200

    added api test case

---
+++ socialeyeser-parent/alerting/trend-detection/src/test/java/it/cybion/socialeyeser/trends/CrisisDetectorTestCase.java
@@ -0,0 +1,30 @@
+package it.cybion.socialeyeser.trends;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * @author Matteo Moci ( matteo (dot) moci (at) gmail (dot) com )
+ */
+public class CrisisDetectorTestCase {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CrisisDetectorTestCase.class);
+
+    @BeforeClass
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterClass
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testName() throws Exception {
+
+    }
+}

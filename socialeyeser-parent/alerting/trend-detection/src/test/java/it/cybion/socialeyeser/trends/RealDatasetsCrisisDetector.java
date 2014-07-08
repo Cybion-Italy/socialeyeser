4947b6f693fdb2e728ad71791ec91f74a437839f
serxhio <serxhiodaja@gmail.com>
2014-07-08 17:29:23 +0200

    tuning parameters for real data

--- socialeyeser-parent/alerting/trend-detection/src/test/java/it/cybion/socialeyeser/trends/RealDatasetsCrisisDetector.java
+++ socialeyeser-parent/alerting/trend-detection/src/test/java/it/cybion/socialeyeser/trends/RealDatasetsCrisisDetector.java
@@ -1,21 +1,43 @@
 package it.cybion.socialeyeser.trends;
 
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.createStrictMock;
+import static org.easymock.EasyMock.expectLastCall;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.reset;
+import static org.easymock.EasyMock.verify;
 import it.cybion.socialeyeser.trends.runners.StreamReplayer;
+
+import java.util.Observable;
+import java.util.Observer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class RealDatasetsCrisisDetector {
+    
+    private static final double ALERT_RATIO_THRESHOLD = 0.1;
+    
+    private static final double ALERT_THRESHOLD = 0.75;
     
     private static final Logger LOGGER = LoggerFactory.getLogger(RealDatasetsCrisisDetector.class);
     
     private static final String DATASETS_DIR = "/Data/crisis_data/ordered/";
     private static final String[] DATASET_NAMES = new String[] { "fiat" };
     
+    private Observer mockObserver;
+    
     // private static final String[] DATASET_NAMES = new String[] { "fiat",
     // "toyota", "prandelli",
     // "unipolsai", "ubibanca", "carige", "mcdonalds" };
+    
+    @BeforeClass
+    public void setUp() {
+    
+        this.mockObserver = createStrictMock(Observer.class);
+    }
     
     @Test
     public void shouldTestCrisisDetectorOnRealDatasets() throws Exception {
@@ -27,16 +49,23 @@ public class RealDatasetsCrisisDetector {
             StreamReplayer replayer = new StreamReplayer(3000000, DATASETS_DIR + crisisName
                     + ".zip");
             
-            CrisisDetector detector = new CrisisDetector(0.8, 0.3);
+            reset(this.mockObserver);
+            mockObserver.update(anyObject(Observable.class), anyObject());
+            expectLastCall().atLeastOnce();
+            replay(this.mockObserver);
             
-            LOGGER.debug("******************* BEGIN OF STREAM *****************");
+            CrisisDetector detector = new CrisisDetector(ALERT_THRESHOLD, ALERT_RATIO_THRESHOLD);
+            
+            LOGGER.debug("*** BEGIN OF STREAM for " + crisisName + " ***");
             try {
                 while (true) {
                     detector.detect(replayer.nextTweet());
                 }
             } catch (Exception e) {
-                LOGGER.debug("******************* END OF STREAM *****************");
+                LOGGER.debug("*** END OF STREAM ***");
             }
+            
+            verify(this.mockObserver);
         }
     }
     
